a77440f4335902c773c9df843fb23c2b35b114dd
yandaki <boayves@gmail.com>
2023-12-15 13:13:49 +0000

    configuration project

--- todolist_api/settings.py
+++ todolist_api/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'core_user',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_filters',
+    'djoser',
+    'rest_framework_simplejwt',
+    
 ]
 
 MIDDLEWARE = [
@@ -78,6 +86,13 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 
